c502b94d6bc98e12d15c1513a583154f06b12df0
pedroquintao <pedro.h.quintao@hotmail.com>
2023-07-29 17:27:40 -0300

    Refatoração nas funções de adicionar listaners
    -Todas as funções que adicionavam os listeners aos botões foram unificadas em uma só função, e tudo referente a essa adição foi trasferido para o código constructors.js

--- main.js
+++ main.js
@@ -1,46 +1,6 @@
 import { constructor } from "./modules/constructors.js";
-import { controller } from "./modules/controllers.js";
 
+constructor.addListeners(constructor.movements);
 
+constructor.showGrid(constructor.arrayGrid);
 
-var arrayGrid = constructor.createInitialGrid(constructor.ROW_NUMBER, constructor.COLUMN_NUMBER)
-
-console.log('%cmain.js line:17 arrayGrid', 'color: #007acc;', arrayGrid);
-constructor.showGrid(arrayGrid);
-
-const rightButton = document.querySelector(".right-button")
-const leftButton = document.querySelector(".left-button")
-const upButton = document.querySelector(".up-button")
-const downButton = document.querySelector(".down-button")
-
-rightButton.addEventListener('click', () => {
-    arrayGrid = controller.pressButton(arrayGrid, "right");
-})
-
-leftButton.addEventListener('click', () => {
-    arrayGrid = controller.pressButton(arrayGrid, "left");
-})
-
-upButton.addEventListener('click', () => {
-    arrayGrid = controller.pressButton(arrayGrid, "up");
-})
-
-downButton.addEventListener('click', () => {
-    arrayGrid = controller.pressButton(arrayGrid, "down");
-})
-
-// rightButton.addEventListener('click', () => {
-//     arrayGrid = controller.pressRightButton(arrayGrid);
-// })
-
-// leftButton.addEventListener('click', () => {
-//     arrayGrid = controller.pressLeftButton(arrayGrid);
-// })
-
-// upButton.addEventListener('click', () => {
-//     arrayGrid = controller.pressUpButton(arrayGrid);
-// })
-
-// downButton.addEventListener('click', () => {
-//     arrayGrid = controller.pressDownButton(arrayGrid);
-// })
--- modules/constructors.js
+++ modules/constructors.js
@@ -5,6 +5,40 @@ const ROW_NUMBER = 4;
 const COLUMN_NUMBER = 4;
 const INITIAL_RANDOM_NUMBERS = 2;
 
+const rightButton = document.querySelector(".right-button")
+const leftButton = document.querySelector(".left-button")
+const upButton = document.querySelector(".up-button")
+const downButton = document.querySelector(".down-button")
+
+var arrayGrid = createInitialGrid(ROW_NUMBER, COLUMN_NUMBER)
+
+const movements = {
+    
+    "right": {
+        "button": rightButton,
+        "move": "right"
+    }
+    ,
+    
+    "left": {
+        "button": leftButton,
+        "move": "left"
+    }
+    ,
+    
+    "up": {
+        "button": upButton,
+        "move": "up"
+    },
+
+    "down": {
+        "button": downButton,
+        "move": "down"
+    }
+}
+
+
+
 function createInitialGrid(row, column) {
 
   let newGrid = []
@@ -23,6 +57,15 @@ function showGrid(grid) {
   controller.isGameOver(grid)? constructor.HTML_GRID.innerHTML = "GAME OVER!" : HTML_GRID.innerHTML = grid.join('<br>')
 }
 
+function addListeners(obj) {
+
+  for(let o in obj) {
+      obj[o]["button"].addEventListener('click', () => {
+          arrayGrid = controller.pressButton(arrayGrid, obj[o]["move"])
+      })
+  }
+}
+
 function createNewRandomPositionElement(grid) {
   const rowIndex = Math.floor(Math.random() * ROW_NUMBER);
   const columnIndex = Math.floor(Math.random() * COLUMN_NUMBER);
@@ -36,7 +79,10 @@ export const constructor = {
   HTML_GRID,
   ROW_NUMBER,
   COLUMN_NUMBER,
+  arrayGrid,
+  movements,
   createInitialGrid,
   showGrid,
+  addListeners,
   createNewRandomPositionElement
 }
